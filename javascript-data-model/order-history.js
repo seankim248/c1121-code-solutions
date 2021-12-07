a1f2a0f8e1b065fb70ee1899523af852feeb38d5
Sean Kim <seankim2480@gmail.com>
2021-12-07 11:13:01 -0800

    javascript-data-model - Sean Kim

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,78 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    orderDelivered: 'Aug 8, 2020',
+    total: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    itemsOrdered: [
+      {
+        deliveryInfo: null,
+        imgURL: 'imageURL',
+        title: 'JavaScript for impatient programmers',
+        author: 'Raushmayer, Dr.Axel',
+        returnWindow: 'Sep 7, 2020',
+        price: 31.55
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    orderDelivered: 'Jul 20, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    itemsOrdered: [
+      {
+        deliveryInfo: 'Your package was delivered. It was handed directly to a resident',
+        imgURL: 'imageURL',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Aug 19, 2020',
+        price: 41.33
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    orderDelivered: 'Jul 7, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    itemsOrdered: [
+      {
+        deliveryInfo: 'Your package was delivered. It was handed directly to a resident',
+        imgURL: 'imageURL',
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: null,
+        returnWindow: 'Aug 5, 2020',
+        price: 15.98
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    orderDelivered: 'Jul 5, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    itemsOrdered: [
+      {
+        deliveryInfo: null,
+        imgURL: 'imageURL',
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        returnWindow: 'Aug 4, 2020',
+        price: 94.95
+      },
+      {
+        deliveryInfo: null,
+        imgURL: 'imageURL',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindow: 'Aug 4, 2020',
+        price: 33.99
+      }
+    ]
+  }
+];
