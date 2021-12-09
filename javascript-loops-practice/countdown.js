2f2ee5f2b538795ec7000e039c6943629e9a0625
Sean Kim <seankim2480@gmail.com>
2021-12-09 13:13:45 -0800

    javascript-loops-practice - Sean Kim

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,1 +1,9 @@
 /* exported countdown */
+function countdown(number) {
+  var numArr = [];
+  while (number >= 0) {
+    numArr.push(number);
+    number--;
+  }
+  return numArr;
+}
--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -1,1 +1,10 @@
 /* exported filterOutStrings */
+function filterOutStrings(values) {
+  var result = [];
+  for (var i = 0; i < values.length; i++) {
+    if (typeof values[i] !== 'string') {
+      result.push(values[i]);
+    }
+  }
+  return result;
+}
