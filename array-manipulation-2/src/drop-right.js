e2f7c00f0b63eb517829aaa89d7bdab79428309e
Sean Kim <seankim2480@gmail.com>
2021-12-10 21:08:08 -0800

    array-manipulation-2 - Sean Kim

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,6 @@
 /* exported dropRight */
+// define function
+function dropRight(array, count) {
+  // push out the items that don't include the items from the end of the list to "count" value
+  return array.slice(0, -count);
+}
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,1 +1,6 @@
 /* exported take */
+// define function
+function take(array, count) {
+  // push out the list of items from the beginning till count
+  return array.slice(0, count);
+}
