359cc8cfde539acc9873b3f440afbe6856809e12
Sean Kim <seankim2480@gmail.com>
2021-12-13 15:01:25 -0800

    WIP

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,8 +1,29 @@
 /* exported equal */
 // define function
 function equal(first, second) {
-  var string1 = first.join('');
-  var string2 = second.join('');
-  if (string1 === string2) return true;
+  // if the length of first list is different from second list return false
+  if (first.length !== second.length) return false;
+  // storage to keep track of true or false value
+  var isTrue = true;
+  // iterate through first length
+  for (var i = 0; i < first.length; i++) {
+    // if item in first list is equal to the same index item in second list,
+    if (first[i] === second[i]) {
+      // keep isTrue equal to true
+      isTrue = true;
+      // otherwise
+    } else {
+      // set isTrue equal to false
+      isTrue = false;
+      // break out of the iteration
+      break;
+    }
+  }
+  // if isTrue is still true,
+  if (isTrue === true) {
+    // return true
+    return true;
+  }
+  // otherwise return false
   return false;
 }
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,13 @@
 /* exported flatten */
+// // define function
+// function flatten(array) {
+//   var arr = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (!Array.isArray(array[i])) {
+//       arr.push(array[i]);
+//     } else {
+//       for (var j = 0; j < array[i].length);
+//     }
+//   }
+//   return arr;
+// }
