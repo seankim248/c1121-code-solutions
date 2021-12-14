3329f1557fbe176371cbcbdab577802f4cbff0ba
Sean Kim <seankim2480@gmail.com>
2021-12-13 17:40:13 -0800

    used for in loop rather than spread operator in omit.js

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,9 +2,12 @@
 // define function
 function omit(source, keys) {
   // create a copy of source storage
-  var newObj = {
-    ...source
-  };
+  var newObj = {};
+  // look through source storage
+  for (var key in source) {
+    // copy key and property over to newObj storage
+    newObj[key] = source[key];
+  }
   // iterate through the keys list
   for (var i = 0; i < keys.length; i++) {
     // if the item in key list shows up in source storage,
