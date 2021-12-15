63e8b19b96d46764ebc161df929159b0f1445ac3
Sean Kim <seankim2480@gmail.com>
2021-12-15 11:30:27 -0800

    javascript-forms - Sean Kim

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,11 @@
+var $form = document.querySelector('form');
+var $inputs = document.querySelector('form').elements;
+
+$form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var obj = {};
+  for (var i = 0; i < $inputs.length; i++) {
+    obj[$inputs[i].name] = $inputs[i].value;
+  }
+  console.log('Message Data:', obj);
+});
