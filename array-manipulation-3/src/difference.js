905088316f5f88bf6457a606afd6625d9de42d4c
Sean Kim <seankim2480@gmail.com>
2021-12-15 13:00:35 -0800

    array-manipulation-3 - Sean Kim

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,26 @@
 /* exported difference */
+// define function
+function difference(first, second) {
+  // storage for empty list
+  var result = [];
+
+  // iterate through first list
+  for (var i = 0; i < first.length; i++) {
+    // if first list item is not included in the second list,
+    if (!second.includes(first[i])) {
+      // put the item into the result list
+      result.push(first[i]);
+    }
+  }
+
+  // iterate through second list
+  for (var j = 0; j < second.length; j++) {
+    // if second list item is not included in the first list,
+    if (!first.includes(second[j])) {
+      // put the item into the result list
+      result.push(second[j]);
+    }
+  }
+  // return result
+  return result;
+}
--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,32 @@
 /* exported intersection */
+// define function
+function intersection(first, second) {
+  // storage for empty list
+  var result = [];
+
+  // if first list length is greater or equal to second list length,
+  if (first.length >= second.length) {
+    // iterate through first list
+    for (var i = 0; i < first.length; i++) {
+      // if first list item is included in the second list,
+      if (second.includes(first[i])) {
+        // put the item into the result list
+        result.push(first[i]);
+      }
+    }
+  }
+
+  // if second list length is greater than first list length,
+  if (second.length > first.length) {
+    // iterate through second list
+    for (var j = 0; j < second.length; j++) {
+      // if second list item is included in the first list,
+      if (first.includes(second[j])) {
+        // put the item into the result list
+        result.push(second[j]);
+      }
+    }
+  }
+  // return result
+  return result;
+}
