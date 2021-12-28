95ae61f348ea1d60d046998dc1039ee48a47d9f2
Sean Kim <seankim2480@gmail.com>
2021-12-28 10:01:01 -0800

    WIP

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,8 +1,5 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-var deck = [];
-var suits = ['♠', '♥', '♣', '♦'];
-var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 var players = [
   {
     name: 'Sean Kim',
@@ -22,19 +19,39 @@ var players = [
   }
 ];
 
-for (var i = 0; i < suits.length; i++) {
-  for (var j = 0; j < ranks.length; j++) {
-    deck.push(ranks[j] + suits[i]);
+function runCardGame(players) {
+  var deck = [];
+  var suits = ['♠', '♥', '♣', '♦'];
+  var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+  for (var i = 0; i < suits.length; i++) {
+    for (var j = 0; j < ranks.length; j++) {
+      deck.push(ranks[j] + suits[i]);
+    }
+  }
+  var shuffledDeck = _.shuffle(deck);
+  for (var k = 0; k < players.length; k++) {
+    players[k].hand.push(shuffledDeck[k], shuffledDeck[k + players.length]);
+  }
+  for (var l = 0; l < players.length; l++) {
+    var playerScores = [];
+    var score = handleScore(players[l].hand);
+    playerScores.push(score);
+  }
+  var highestScoreIndex = playerScores.indexOf(highestScore(playerScores));
+
+  if (tieBreaker(playerScores).length === 0) {
+    console.log(players[highestScoreIndex].name);
+  } else {
+    for (var m = 0; m < tieBreaker(playerScores); m++) {
+      var remainingPlayers = [];
+      remainingPlayers.push(players[tieBreaker(playerScores[m])]);
+    }
+    runCardGame(remainingPlayers);
   }
 }
 
-var shuffledDeck = _.shuffle(deck);
-
-for (var k = 0; k < 4; k++) {
-  players[k].hand.push(shuffledDeck[k], shuffledDeck[k + 4]);
-}
-
-var playerScores = [handleScore(players[0].hand), handleScore(players[1].hand), handleScore(players[2].hand), handleScore(players[3].hand)];
+// var playerScores = [handleScore(players[0].hand), handleScore(players[1].hand), handleScore(players[2].hand), handleScore(players[3].hand)];
 
 function handleScore(cards) {
   var total = 0;
@@ -56,8 +73,6 @@ function highestScore(scores) {
   return highest;
 }
 
-var highestScoreIndex = playerScores.indexOf(highestScore(playerScores));
-
 function tieBreaker(scores) {
   var tieIndices = [];
   for (var i = 0; i < scores.length; i++) {
@@ -72,20 +87,16 @@ function tieBreaker(scores) {
   return tieIndices;
 }
 
-function logTieBreaker(arr) {
-  var names = '';
-  for (var m = 0; m < arr.length; m++) {
-    if (m === arr.length - 1) {
-      names += players[arr[m]].name;
-    } else {
-      names += players[arr[m]].name + ' and ';
-    }
-  }
-  return names + ' tied!';
-}
+// function logTieBreaker(arr) {
+//   var names = '';
+//   for (var m = 0; m < arr.length; m++) {
+//     if (m === arr.length - 1) {
+//       names += players[arr[m]].name;
+//     } else {
+//       names += players[arr[m]].name + ' and ';
+//     }
+//   }
+//   return names + ' tied!';
+// }
 
-if (tieBreaker(playerScores).length === 0) {
-  console.log(players[highestScoreIndex].name);
-} else {
-  console.log(logTieBreaker(tieBreaker(playerScores)));
-}
+runCardGame(players);
