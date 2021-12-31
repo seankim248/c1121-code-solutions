1afe708e4bec270632e97cb9a36a443161841105
Sean Kim <seankim2480@gmail.com>
2021-12-31 12:29:10 -0800

    WiP

---
+++ javascript-carousel/main.js
@@ -0,0 +1,12 @@
+// var leftArrow = document.querySelector('.fa-chevron-left');
+// var rightArrow = document.querySelector('.fa-chevron-right');
+// var circles = document.querySelectorAll('fa-circle');
+
+// var imgArr = ['001', '004', '007', '025', '039'];
+
+// leftArrow.addEventListener('click', function (e) {
+//   console.log(e.target.nextSibling);
+//   // for (var i = 0; i < imgArr.length; i++) {
+//   //   if (e.target)
+//   // }
+// });
