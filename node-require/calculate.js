871605a64be22f406a9f6d68bc29b2683ccd8cd8
Sean Kim <seankim2480@gmail.com>
2022-01-19 12:26:14 -0800

    node-require

---
+++ node-require/calculate.js
@@ -0,0 +1,24 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+var num1 = parseInt(process.argv[2]);
+var num2 = parseInt(process.argv[4]);
+var operator = process.argv[3];
+
+if (operator === 'plus') {
+  console.log(add(num1, num2));
+}
+
+if (operator === 'minus') {
+  console.log(subtract(num1, num2));
+}
+
+if (operator === 'times') {
+  console.log(multiply(num1, num2));
+}
+
+if (operator === 'over') {
+  console.log(divide(num1, num2));
+}
