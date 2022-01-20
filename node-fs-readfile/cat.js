8d805a9a769041f1ec19892ce37f0666e460667c
Sean Kim <seankim2480@gmail.com>
2022-01-19 16:48:52 -0800

    finished level 1, 2, 3

---
+++ node-fs-readfile/cat.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+for (let i = 2; i < process.argv.length; i++) {
+  fs.readFile(`./${process.argv[i]}`, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+}
