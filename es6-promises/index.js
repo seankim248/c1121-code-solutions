72a439232d9b64bf59abf8b4cb218c8e6880cf72
Sean Kim <seankim2480@gmail.com>
2022-01-28 19:13:11 +0000

    updated format of promise

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -1,9 +1,10 @@
 const takeAChance = require('./take-a-chance');
 
 const promise = takeAChance('Sean');
-promise.then(value => {
-  console.log(value);
-});
-promise.catch(error => {
-  console.error(error.message);
-});
+promise
+  .then(value => {
+    console.log(value);
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
