3d22836807002f7b415e82d8414b9d235e6b0950
Sean Kim <seankim2480@gmail.com>
2022-02-02 11:31:28 -0800

    react-function-components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton() {
+  return <button>Click Me!</button>;
+}
+
+const element = <CustomButton/>;
+
+ReactDOM.render(element, document.getElementById('root'));
