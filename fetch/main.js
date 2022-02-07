315ac4154cfbf43dedd16513cfb4157a2ba05962
Sean Kim <seankim2480@gmail.com>
2022-02-07 19:47:01 +0000

    added changes

--- fetch/main.js
+++ fetch/main.js
@@ -1,5 +1,3 @@
-const fetchResponse = fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://jsonplaceholder.typicode.com/users')
   .then(res => res.json())
   .then(data => console.log(data));
-
-console.log(fetchResponse);
